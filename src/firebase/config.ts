35457316874046b86103c1d137d210003b939c7f
rob <heidlessemail16@gmail.com>
2025-10-25 12:58:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "heidless-firebase",
+  "appId": "1:232488530911:web:97a9cc1f261972d3110a6a",
+  "apiKey": "AIzaSyAQ1mxB2uj86qOAXlT_mUQitK61PQiqXYY",
+  "authDomain": "heidless-firebase.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "232488530911"
+};
