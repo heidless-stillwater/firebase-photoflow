2439efe0ef657b7bfe880bdaed9742bd2f88abea
rob <heidlessemail16@gmail.com>
2025-10-25 13:04:35 +0000

    Modify the app so that app environment variables defined in a .env file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,12 @@
+// This file is used to hold the Firebase config object.
+// It is populated by environment variables.
+
 export const firebaseConfig = {
-  "projectId": "heidless-firebase",
-  "appId": "1:232488530911:web:97a9cc1f261972d3110a6a",
-  "apiKey": "AIzaSyAQ1mxB2uj86qOAXlT_mUQitK61PQiqXYY",
-  "authDomain": "heidless-firebase.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "232488530911"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
